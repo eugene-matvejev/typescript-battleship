5580af9e6b751494c243c86942c91cae75ebdbc6
Eugene Matvejevs <eugene.matvejev@gmail.com>
2016-07-22 10:54:03 +0100

    v0.2.16 convert Page.manager to TS

--- src/js/manager/page.manager.ts
+++ src/js/manager/page.manager.ts
@@ -1,38 +1,30 @@
 class PageMgr {
-    protected modalMgr   : ModalMgr;
-    protected popupMgr   : PopupMgr;
-    protected $docTitle  : any;
-    protected $loading   : any;
-    protected $sidebar   : any;
-    protected $content   : any;
-    protected $pageTitle : any;
+    protected modalMgr: ModalMgr;
+    protected popupMgr: PopupMgr;
+    protected $docTitle: any;
+    protected $loading: any;
+    protected $sidebar: any;
+    protected $content: any;
+    protected $pageTitle: any;
 
     constructor() {
-        this.modalMgr   = new ModalMgr();
-        this.popupMgr   = new PopupMgr();
-        this.$docTitle  = $('head>title');
-        this.$loading   = $('.page-loading');
-        this.$sidebar   = $('.page-sidebar');
-        this.$content   = $('.page-content');
+        this.modalMgr = new ModalMgr();
+        this.popupMgr = new PopupMgr();
+        this.$docTitle = $('head>title');
+        this.$loading = $('.page-loading');
+        this.$sidebar = $('.page-sidebar');
+        this.$content = $('.page-content');
         this.$pageTitle = this.$content.find('.page-section-title');
     }
 
-    /**
-     * @returns {PageMgr}
-     */
-    toggleSidebar() {
+    toggleSidebar(): PageMgr {
         this.$sidebar.toggleClass('toggled');
         this.$content.toggleClass('toggled');
 
         return this;
     }
 
-    /**
-     * @param {Element} el
-     *
-     * @returns {PageMgr}
-     */
-    switchSection(el) {
+    switchSection(el: HTMLElement): PageMgr {
         let section = el.getAttribute('data-section');
         switch (section) {
             case 'game-current-area':
@@ -47,34 +39,22 @@ class PageMgr {
         return this;
     }
 
-    /**
-     * @returns {PageMgr}
-     */
-    hideAll() {
+    hideAll(): PageMgr {
         this.$content.find('.container-fluid > .row > div:not(#notification-area)').addClass('hidden');
         this.$sidebar.find('li:not(.sidebar-brand)').removeClass('selected');
 
         return this;
     }
 
-    /**
-     * @param {string} id
-     *
-     * @returns {PageMgr}
-     */
-    show(id) {
+
+    show(id: string): PageMgr {
         this.$content.find(`div#${id}`).removeClass('hidden');
         this.$sidebar.find(`li[data-section="${id}"]`).addClass('selected');
 
         return this;
     }
 
-    /**
-     * @param {string} text
-     *
-     * @returns {PageMgr}
-     */
-    toggleTitle(text) {
+    toggleTitle(text: string): PageMgr {
         let prefix = this.$sidebar.find('.page-header').text();
 
         this.$docTitle.text(`${prefix} :: ${text}`);
@@ -83,12 +63,7 @@ class PageMgr {
         return this;
     }
 
-    /**
-     * @param {boolean} [enable]
-     *
-     * @returns {PageMgr}
-     */
-    loadingMode(enable) {
+    loadingMode(enable?: boolean): PageMgr {
         this.modalMgr.updateHTML('').hide();
 
         this.$loading.addClass('hidden');
