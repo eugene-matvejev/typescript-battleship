dcf75cfbfba60598a88544bf162b22889ac40c48
Eugene Matvejevs <eugene.matvejev@gmail.com>
2016-07-22 20:45:37 +0100

    v1.0.0 full TypeScript support; cleanup;

--- config.rb
+++
@@ -1,8 +0,0 @@
-require 'compass/import-once/activate'
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-
-sass_dir = "src/scss/"
-css_dir = "web/assets/css"
--- src/reference.ts
+++ src/reference.ts
@@ -1,7 +1,3 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 
-// declare module "jquery" {
-//     export = $;
-// }
-// declare var jQuery: JQueryStatic;
 declare var $: JQueryStatic;
