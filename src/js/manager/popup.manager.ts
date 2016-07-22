6b8cb77cb097b5966b2651050d48ca9b924253a3
Eugene Matvejevs <eugene.matvejev@gmail.com>
2016-07-22 11:10:33 +0100

    v0.2.17 convert Popup.manager to TS

--- src/js/manager/popup.manager.ts
+++ src/js/manager/popup.manager.ts
@@ -1,29 +1,20 @@
 class PopupMgr {
-    private $html : any;
-    private $content : any;
+    private $html: any;
+    private $content: any;
 
     constructor() {
-        this.$html    = $('#notification-area');
+        this.$html = $('#notification-area');
         this.$content = this.$html.find('.notification-content');
     }
 
-    /**
-     * @param {string} text
-     * @param {string} type
-     *
-     * @returns {PopupMgr}
-     */
-    show(text, type) {
+    show(text: string, type: string): PopupMgr {
         this.$content.html(text);
         this.$html.removeClass().addClass(`alert alert-${type}`);
 
         return this;
     }
 
-    /**
-     * @returns {PopupMgr}
-     */
-    hide() {
+    hide(): PopupMgr {
         this.$html.addClass('hidden');
 
         return this;
