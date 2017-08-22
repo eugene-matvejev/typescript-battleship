e40a08f16de5fe43597b12284413cc5fbadf8554
Eugene Matvejev <jevgenijs.matvejevs@wipro.com>
2017-08-22 15:55:07 +0100

    drop configuration.storage.ts

--- src/js/configuration.storage.ts
+++
@@ -1,6 +0,0 @@
-class Configuration {
-    protected static requestHost = 'api.game.local/app_dev.php';
-    protected static requestProtocol = 'http';
-    /** in milliseconds */
-    protected static requestTimeout = 5000;
-}
