0f566c665b8e4d5e53e7e85dcc9a6d405d00b6cf
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-08-25 10:37:50 +0100

    Update service-worker-registry.js

--- web/service-worker-registry.js
+++ web/service-worker-registry.js
@@ -1,23 +1,9 @@
 if ('serviceWorker' in navigator) {
-    // Override the default scope of '/' with './', so that the registration applies
-    // to the current directory and everything underneath it.
     navigator.serviceWorker.register('/service-worker-api-faker.js', {scope: './'}).then(function (registration) {
-        // At this point, registration has taken place.
-        // The service worker will not handle requests until this page and any
-        // other instances of this page (in other tabs, etc.) have been
-        // closed/reloaded.
-        // document.querySelector('#status').textContent = 'succeeded';
-        console.log('successfull service worked');
+        console.log('SW successfully installed');
     }).catch(function (error) {
-        // Something went wrong during registration. The service-worker.js file
-        // might be unavailable or contain a syntax error.
-        // document.querySelector('#status').textContent = error;
-        console.log('errored service worked', error);
+        console.log('SW installation failed', error);
     });
 } else {
-    // The current browser doesn't support service workers.
-    var aElement = document.createElement('a');
-    aElement.href = 'http://www.chromium.org/blink/serviceworker/service-worker-faq';
-    // aElement.textContent = 'unavailable';
-    // document.querySelector('#status').appendChild(aElement);
+    console.log('browser do not support SW');
 }
