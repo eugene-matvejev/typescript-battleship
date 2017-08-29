95a67eaa4e2347d84334c9eb3699155868baf41d
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-08-29 13:48:24 +0100

    Delete service-worker-registry.js

--- web/service-worker-registry.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker-api-faker.js', {scope: './'}).then(function (registration) {
-        console.log('SW successfully installed');
-    }).catch(function (error) {
-        console.log('SW installation failed', error);
-    });
-} else {
-    console.log('browser do not support SW');
-}
